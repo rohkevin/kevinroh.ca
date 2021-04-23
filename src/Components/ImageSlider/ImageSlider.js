a56ff83b803aff12f2e54fc9172c8ef2ff08dadd
rohkevin <kevinrohmail@gmail.com>
2021-04-22 22:20:48 -0400

    Update functionality for image slider and slider navigation

--- src/Components/ImageSlider/ImageSlider.js
+++ src/Components/ImageSlider/ImageSlider.js
@@ -1,51 +1,69 @@
 import React, { useEffect, useState } from 'react'
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa'
 import './ImageSlider.scss'
-
+// if (index === imageIndex - 1 || (imageIndex === 0 && index === images.length -1)) {
+//               position = 'last-slide';
+//             }
 
 function ImageSlider({ projectName, sliderImages }) {
   const [images, setImages] = useState(sliderImages);
   const [imageIndex, setImageIndex] = useState(0);
+  useEffect(() => {
+    const lastIndex = images.length - 1;
+    if (imageIndex < 0){
+      setImageIndex(lastIndex);
+    }
+    if (imageIndex > lastIndex){
+      setImageIndex(0);
+    }
+  }, [imageIndex, images])
 
-  
-
-  const prevReview = () => {
-    setImageIndex((prevIndex) => {
-      if (prevIndex!==0) {
-        return prevIndex-1;
-      } else {
-        return images.length-1;
-      }
-    });
-  }
-  const nextReview = () => {
-    let currentIndex = imageIndex;
-    let nextIndex = ++currentIndex % images.length;
-    setImageIndex(nextIndex);
-  }
   const handleChangeImageLocation = (index) => {
     setImageIndex(index);
   }
-  return (
-    <div id="image-slider">
-      <button onClick={prevReview} className="img-btn left"><FaChevronLeft /></button>
-      <figure className="image-container">
-        <img src={`/assets/Images/Projects/${projectName}${images[imageIndex]}`} alt={`TEMPNAME`} />
-      </figure>
-      <div className="slider-location">
-        {images.map((image, index) => {
-          return (
-            <button 
-              key={`slider${index}`} 
-              className={ index === imageIndex ? "active-location location" : "location"} 
-              onClick={()=>handleChangeImageLocation(index)}
-            />
-          )
-        })}
+  
+  if (images){
+    return (
+      <div id="image-slider">
+        <div className="image-container">
+          {images.map((image, index) => {
+            let position = 'next-slide';
+            if (index === imageIndex) {
+              position = 'active-slide';
+            }
+            
+            if (index === imageIndex -1 || (imageIndex === 0 && index === images.length -1) || imageIndex > index) {
+              position = 'last-slide';
+            }
+            if (imageIndex < index) {position='next-slide'};
+            return (
+              <figure className={`image-slide ${position}`} key={projectName + index}>
+                <img src={`/assets/Images/Projects/${projectName}${image}`} alt={`TEMPNAME`} />
+              </figure>
+            )
+          })}
+        </div>
+
+        <div className="slider-location">
+          {images.map((image, index) => {
+            return (
+              <button 
+                key={`slider${index}`} 
+                className={ index === imageIndex ? "active-location location" : "location"} 
+                onClick={()=>handleChangeImageLocation(index)}
+              />
+            )
+          })}
+        </div>
+        <button onClick={() => setImageIndex(imageIndex -1)} className="img-btn left"><FaChevronLeft /></button>
+        <button onClick={() => setImageIndex(imageIndex + 1)} className="img-btn right"><FaChevronRight /></button>
       </div>
-      <button onClick={nextReview} className="img-btn right"><FaChevronRight /></button>
-    </div>
-  )
+    )
+  } else {
+    return (
+      <div>Images coming soon</div>
+    )
+  }
 }
 
 export default ImageSlider
--- src/tempdata.js
+++ src/tempdata.js
@@ -41,7 +41,9 @@ export const projects = [
     sliderImages: [
       "/1.png",
       "/2.png",
-      "/3.png"
+      "/3.png",
+      "/4.png",
+      "/5.png",
     ]
   },
   {

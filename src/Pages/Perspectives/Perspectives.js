07ad205724cafb426da8849009360dd3a5b0a95c
rohkevin <kevinrohmail@gmail.com>
2021-04-25 17:22:36 -0400

    Add responsive design to Perspectives page, include modal image viewer feature

--- src/Pages/Perspectives/Perspectives.js
+++ src/Pages/Perspectives/Perspectives.js
@@ -1,37 +1,78 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Perspectives.scss'
 import Masonry from 'react-masonry-css'
 import {photos} from '../../tempdata'
 
 function Perspectives() {
+  const [images, setImages] = useState(photos);
+  const [modalImage, setModalImage] = useState(null);
+  const [imageModal, setImageModal] = useState(false);
+
   const breakpoints = {
     default: 3,
-    1100: 2,
-    700: 1
+    1280: 2,
+    900: 1
   };
+
+  useEffect(() => {
+    const handleEsc = (e) => {
+      if (e.keyCode === 27) {
+        setImageModal(false);
+      }
+    }
+    window.addEventListener('keydown', handleEsc);
+    return () => {
+      window.removeEventListener('keydown', handleEsc);
+    }
+  }, []);
+
+  const handleOutsideClick = (e) => {
+    if (e.target.classList.contains('image-overlay')){
+      setImageModal(false);
+    }
+  }
+
+  const handleImageClick = (img) => {
+    setImageModal(true);
+    setModalImage(img);
+  }
 
   return (
     <main id="perspectives">
-      <div className="page full full-mid">
-        <h1>Perspectives</h1>
-        <p className="subtitle1">It's incredible to know that everyone interprets the world in their own way. Let me share with you a bit of what I've seen from my perspective.</p>
+      <div className="max-width">
+        <div className="page full full-mid">
+          <h1>Perspectives</h1>
+          <p className="subtitle1">It's incredible to know that everyone interprets the world in their own way. Let me share with you a bit of what I've seen from my perspective.</p>
 
-      </div>
+        </div>
 
-      <div className="perspectives-container">
-        <Masonry
-          breakpointCols={breakpoints}
-          className="my-masonry-grid"
-          columnClassName="my-masonry-grid_column"
-        >
-        {photos.map(photo => {
-          return (
-            <figure key={photo.name} className="perspectives-figure">
-              <img src={`/assets/Images${photo.path}`} alt={photo.name} />
-            </figure>
-          )
-        })}
-        </Masonry>
+        {
+          imageModal && modalImage ? 
+            <div className={imageModal && modalImage ? 'image-overlay' : 'display-none'} onClick={handleOutsideClick}>
+              <figure>
+                <img src={`/assets/Images${modalImage.path}`} alt={modalImage.name} onClick={()=>setImageModal(false)}/>
+              </figure>
+            </div>
+          :
+            null
+        }
+
+        <div className="perspectives-container">
+          <Masonry
+            breakpointCols={breakpoints}
+            className="my-masonry-grid"
+            columnClassName="my-masonry-grid_column"
+          >
+          {images.map(image => {
+            return (
+              <figure key={image.name} className="perspectives-figure" >
+                <img src={`/assets/Images${image.path}`} alt={image.name} onClick={()=> handleImageClick(image)}/>
+              </figure>
+            )
+          })}
+          </Masonry>
+        </div>
+
       </div>
     </main>
   )

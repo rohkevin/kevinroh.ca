0a038a0e49533969694f5a197ed214d7a3017911
rohkevin <kevinrohmail@gmail.com>
2021-04-25 16:19:31 -0400

    Add responsive design for Works page

--- src/Pages/Works/Works.js
+++ src/Pages/Works/Works.js
@@ -8,22 +8,23 @@ function Works() {
   const { projectsData } = useGlobalContext();
   return (
     <main id="works" className="page" >
-      <section className="full full-mid works-intro">
-        <h1>Works</h1>
-        <p className="subtitle1">I've worked on a multitude of applications over my coding career, as well as passion projects and helping friends build their personal brands.</p>
-        <p className="subtitle1">You can check them out here or go straight to my <a className="highlight" href="https://github.com/rohkevin" target="_blank" rel="noreferrer noopener">Github</a>.</p>
-      </section>
-      {/* Need to figure out how to dynamically load image based on screen size, my guess is useEffect */}
-      <section className="works-container">
-        {
-        projectsData.map(project => {
-          return (
-            
-            <ProjectPreview key={project.name} img={project.imageMed} {...project}/>
-          )
-        })
-      }
-      </section>
+      <div className="max-width">
+        <section className="full full-mid works-intro">
+          <h1>Works</h1>
+          <p className="subtitle1">I've worked on a multitude of applications over my coding career, as well as passion projects and helping friends build their personal brands.</p>
+          <p className="subtitle1">You can check them out here or go straight to my <a className="highlight" href="https://github.com/rohkevin" target="_blank" rel="noreferrer noopener">Github</a>.</p>
+        </section>
+        {/* Need to figure out how to dynamically load image based on screen size, my guess is useEffect */}
+        <section className="works-container">
+          {
+          projectsData.map((project, index) => {
+            return (
+              <ProjectPreview key={project.name} index={index} img={project.imageMed} {...project}/>
+            )
+          })
+        }
+        </section>
+      </div>
     </main>
   )
 }

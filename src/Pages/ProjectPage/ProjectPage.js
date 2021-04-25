a393b56a30e9e788d382c525beca25d1b1f836da
rohkevin <kevinrohmail@gmail.com>
2021-04-25 18:22:54 -0400

    Add responsive design for project page

--- src/Pages/ProjectPage/ProjectPage.js
+++ src/Pages/ProjectPage/ProjectPage.js
@@ -26,22 +26,25 @@ const ProjectPage = () => {
 
     return (
       <main className="page" id="project-page">
-        <Link to="/works" className="back-to"><FaChevronLeft /><p className="url-link">Back to Works</p></Link>
-        <h1 className="h2">{name}</h1>
-        <div className="project-subheader">
-          <p className="subtitle1 stack">{stack}</p>
-          <a href={github} target="_blank" rel="noopener noreferrer" ><FaGithub className="preview-icons" /></a>
-          <a href={live} target="_blank" rel="noopener noreferrer" ><FaExternalLinkAlt className="preview-icons" /></a>
-  
+        <div className="max-width">
+          <Link to="/works" className="back-to"><FaChevronLeft /><p className="url-link">Back to Works</p></Link>
+          <h1 className="h2">{name}</h1>
+          <div className="project-subheader">
+            <p className="subtitle1 stack">{stack}</p>
+            <a href={github} target="_blank" rel="noopener noreferrer" ><FaGithub className="preview-icons" /></a>
+            <a href={live} target="_blank" rel="noopener noreferrer" ><FaExternalLinkAlt className="preview-icons" /></a>
+    
+          </div>
+          <p className="subtitle1 summary">{summary}</p>
+
+          <ImageSlider projectName={projectName} sliderImages={sliderImages} />
+
+          {description.map((line, index)=>(
+            <Fragment key={`descriptionline${index}`}>
+              <p className="body1">{line}</p><br/>
+            </Fragment>))}
+
         </div>
-        <p className="subtitle1 summary">{summary}</p>
-
-        <ImageSlider projectName={projectName} sliderImages={sliderImages} />
-
-        {description.map((line, index)=>(
-          <Fragment key={`descriptionline${index}`}>
-            <p className="body1">{line}</p><br/>
-          </Fragment>))}
       </main>
     )
 

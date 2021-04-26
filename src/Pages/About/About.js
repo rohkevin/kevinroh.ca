05093a5a8ae31998d2eaeff90285d772f65b1cf6
rohkevin <kevinrohmail@gmail.com>
2021-04-25 21:25:44 -0400

    Add responsive design for About page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -8,47 +8,55 @@ const kevams = '/assets/Images/DesktopVersion/kevinatamsterdam.JPG';
 const kevsev = '/assets/Images/DesktopVersion/kevinatsevilla.JPG'
 const kevgra = '/assets/Images/DesktopVersion/kevinatgranada.jpg'
 
+
 function About() {
   const { openContact } = useGlobalContext();
+
   return (
-    <main>
-      <section className="about-header">
-        <figure>
-          <img src={kevimg} alt="Kevin" />
-        </figure>
-        <div className="page">
-          <h1>About Kevin</h1>
-          <p className="subtitle1 italic">"The future is something which everyone reaches at the rate of sixty minutes an hour, whatever he does, whoever he is."</p>
-          <p className="subtitle1">C. S. Lewis</p>
-        </div>
-      </section>
+    <main className='page-nomargin'>
+        <section className="about-header">
+          <figure>
+            <img src={kevimg} alt="Kevin" />
+          </figure>
+          <div className="page">
+            <div className="max-width about-description">
+              <h1>About Kevin</h1>
+              <p className="subtitle1 italic">"The future is something which everyone reaches at the rate of sixty minutes an hour, whatever he does, whoever he is."</p>
+              <p className="subtitle1">C. S. Lewis</p>
+            </div>
+          </div>
+        </section>
 
-      <section className="page about-container">
-        <p className="body1">An aspiring solo sky-diver, good-vibes enthusiast, and an all-around-the-world backpacker, I strive to be see life from as many perspectives possible.</p>
-        <br/>
-        <p className="body1">My love for life translates directly into my daily works. Whether it was engineering solutions to manufacturing problems, to currently programming functionally exceptional applications, every project is an opportunity to learn something new. </p>
-        <br/>
-        <p className="body1">Daily I am reminded on the wealth of knowledge left behind by what others have built to date. On this small but beautiful planet, I am grateful to have the opportunity to contribute to better-ing our world. Today gives another chance to be a part of unending change, and to relentlessly chase our dreams so they become reality.</p>
-        <br/>
-        <p className="body1">Look around and feel free to drop a message!</p>
-        <br/>
-        <p className="body1"><span onClick={openContact} className=" highlight">Get in touch</span></p>
-      </section>
+        <section className="page about-container">
+          <div className="max-width">
+            <p className="body1">An aspiring solo sky-diver, good-vibes enthusiast, and an all-around-the-world backpacker, I strive to be see life from as many perspectives possible.</p>
+            <br/>
+            <p className="body1">My love for life translates directly into my daily works. Whether it was engineering solutions to manufacturing problems, to currently programming functionally exceptional applications, every project is an opportunity to learn something new. </p>
+            <br/>
+            <p className="body1">Daily I am reminded on the wealth of knowledge left behind by what others have built to date. On this small but beautiful planet, I am grateful to have the opportunity to contribute to better-ing our world. Today gives another chance to be a part of unending change, and to relentlessly chase our dreams so they become reality.</p>
+            <br/>
+            <p className="body1">Look around and feel free to drop a message!</p>
+            <br/>
+            <p className="body1"><span onClick={openContact} className=" highlight">Get in touch</span></p>
 
-      <section className="about-container">
-        <figure>
-          <img src={kevams} alt="Bikes and canals"/>
-        </figure>
+          </div>
+        </section>
 
-        <figure>
-          <img src={kevgra} alt="At the Alhambra in Granada, Spain"/>
-        </figure>
-    
-        <figure>
-          <img src={kevsev} alt="Plaza de Espana "/>
-        </figure>
-    
-      </section>
+        <section className="about-container">
+          <div className="max-width about-photos">
+            <figure>
+              <img src={kevams} alt="Bikes and canals"/>
+            </figure>
+
+            <figure>
+              <img src={kevgra} alt="At the Alhambra in Granada, Spain"/>
+            </figure>
+        
+            <figure>
+              <img src={kevsev} alt="Plaza de Espana "/>
+            </figure>
+          </div>
+        </section>
 
       </main>
   )

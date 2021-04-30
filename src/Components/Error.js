27535265db56cca024400d0eca2274090bf21f99
rohkevin <kevinrohmail@gmail.com>
2021-04-29 21:23:37 -0400

    Update Error component page

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Error() {
   return (
-    <div>
-      Error...
+    <div className="page full full-mid">
+      <h1>Error</h1>
+      <p className="subtitle1">This page does not exist! Click here to go back <Link to="/" className="highlight">Home</Link></p>
     </div>
   )
 }

4c6bcff01cbdc1ae9b48519133f05ba59d5f8437
rohkevin <kevinrohmail@gmail.com>
2021-05-30 11:46:27 -0400

    update about text and styling

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -22,21 +22,30 @@ function About() {
           <div className="page">
             <div className="max-width about-description">
               <h1>About Kevin</h1>
-              <p className="subtitle1 italic">"The future is something which everyone reaches at the rate of sixty minutes an hour, whatever he does, whoever he is."</p>
-              <p className="subtitle1">C. S. Lewis</p>
+              <p className="subtitle1 italic">"Yesterday is gone. Tomorrow has not yet come. We have only today. Let us begin."</p>
+              <p className="subtitle1">Mother Teresa</p>
             </div>
           </div>
         </section>
 
         <section className="page about-container">
           <div className="max-width">
-            <p className="body1">An aspiring solo sky-diver, good-vibes enthusiast, and an all-around-the-world backpacker, I strive to see life from as many perspectives as possible.</p>
+            <p className="body1">Hello! My name is Kevin and I have a lot of fun creating things on the internet. My interest for web development started back in 2008 when journaling on the web consisted of 30 minutes of writing and 3 hours of making it look good.</p>
             <br/>
-            <p className="body1">My love for life translates directly into my daily works. Whether it was engineering solutions to manufacturing problems, to currently programming functionally exceptional applications, every project is an opportunity to learn something new.</p>
+            <p className="body1">Fast forward to today, I've had the privilege of creating multple accessible and inclusive web applications for clients and friends. I love the unique challenges that come with each project, and every hair-pulling moment results in an absolutely rewarding outcome.</p>
             <br/>
-            <p className="body1">Daily I am reminded on the wealth of knowledge left behind by what others have built to date. On this small but beautiful planet, I am grateful to have the opportunity to contribute to better-ing our world. Today gives another chance to be a part of unending change, and to relentlessly chase our dreams so they become reality.</p>
+            <p className="body1">Currently I am working on a range of projects including:</p>
+            <ul>
+              <li>Guess Who online game with chat feature using SocketIO</li>
+              <li>E-commerce platform for a client's online clothing brand</li>
+              <li>Learning Typescript</li>
+            </ul>
             <br/>
-            <p className="body1">Look around and feel free to drop a message!</p>
+            <p className="body1">Outside of work I am an aspiring solo sky-diver, an around-the-world backpacker*, and an apostle for good-vibes. My father taught me from a young age to live life from as many perspectives as possible. My goal is to witness what was left behind by generations, and that comes with knowing the people of today, and what connects us for who we are. Every day I strive to be a part of history that leaves a positive impact for the future!</p>
+            <br/>
+            <p className="body1">Look around and feel free to drop a message.</p>
+            <br/>
+            <p style={{fontSize: '0.8rem', color: 'var(--url-link-color)'}}>*Let me know if you have travel tips for Australia or Peru!</p>
             <br/>
             <p className="body1"><span onClick={openContact} className=" highlight">Get in touch</span></p>
 

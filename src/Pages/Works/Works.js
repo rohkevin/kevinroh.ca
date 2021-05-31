a439d039c0a3d3a4a567d3ae1f75104f24b17f80
rohkevin <kevinrohmail@gmail.com>
2021-05-31 10:40:05 -0400

    Wording update

--- src/Pages/Works/Works.js
+++ src/Pages/Works/Works.js
@@ -14,7 +14,7 @@ function Works() {
       <div className="max-width">
         <section className="full full-mid works-intro">
           <h1>Works</h1>
-          <p className="subtitle1">I've worked on a multitude of applications over my coding career, as well as passion projects and helping friends build their personal brands.</p>
+          <p className="subtitle1">I've worked on multiple applications over my coding career, as well as passion projects and helping friends build their personal brands.</p>
           <p className="subtitle1">You can check them out here or go straight to my <a className="highlight" href="https://github.com/rohkevin" target="_blank" rel="noreferrer noopener">Github</a>.</p>
         </section>
         {/* Need to figure out how to dynamically load image based on screen size, my guess is useEffect */}
